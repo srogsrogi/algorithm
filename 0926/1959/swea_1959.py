69b9f303138f2ed84de04f095a6d3fb895db6013
srogsrogi <han970112@gmail.com>
2025-09-26 19:38:40 +0900

    0926

---
+++ 0926/1959/swea_1959.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    ai = list(map(int, input().split()))
+    bj = list(map(int, input().split()))
+
+    if N > M:
+        for i in range()
+
+
+
+    # print(f'#{tc} {result})
