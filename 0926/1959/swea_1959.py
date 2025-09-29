21603ccb89009b9fc05d99d94ddfdd0b11e2253e
srogsrogi <han970112@gmail.com>
2025-09-29 19:55:34 +0900

    0926

--- 0926/1959/swea_1959.py
+++ 0926/1959/swea_1959.py
@@ -8,8 +8,46 @@ for tc in range(1, T+1):
     bj = list(map(int, input().split()))
 
     if N > M:
-        for i in range()
+        ai, bj = bj, ai
+        N, M = M, N
+
+    best = 0
+
+    for i in range(M-N+1):
+        prefix_sum = 0
+        for j in range(N):
+            prefix_sum += ai[j] * bj[i+j]
+
+        if best < prefix_sum:
+            best = prefix_sum
+
+    print(f'#{tc} {best}')
 
 
 
-    # print(f'#{tc} {result})
+
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    best = 0
+
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+            prefix_sum = 0
+            for k in range(i, i+M):
+                for l in range(j, j+M):
+                    prefix_sum += arr[k][l]
+
+            if prefix_sum > best:
+                best = prefix_sum
+
+
+    print(f'#{tc} {best}')
+
+
+    # print(f'#{tc} {result}')
